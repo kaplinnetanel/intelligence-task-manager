5c3e80ca80ee71eae77c5228566157b6a11a94ca
נתנאל קאפלין <kaplinnetanel10@gmail.com>
2026-06-17 12:46:06 +0300

    Add initial database connection and agent management classes

---
+++ Business_Logic_db.py
@@ -0,0 +1,5 @@
+from database.db_connection import DB_connection
+
+connection = DB_connection()
+
+connection.create_database()
---
+++ database/agent_db.py
@@ -0,0 +1,12 @@
+from db_connection import DB_connection
+
+agent_conn = DB_connection()
+
+class DB_connection:
+    agent_conn = DB_connection()
+    def create_agent(self,data):
+        pass
+    def get_all_agents(self):
+        conn = self.agent_conn.
+    def get_agent_by_id(id):
+        pass
